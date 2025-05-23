81c9c96a88abaa337c328d0cb05a3bcebfdab443
PedroMVillar <pedro.villar@mi.unc.edu.ar>
2025-05-22 21:00:37 -0300

    refactor: implementar métodos para realizar peticiones GET y devolver respuestas en formato XML y JSON

--- src/httpRequest/httpRequester.java
+++ src/httpRequest/httpRequester.java
@@ -1,5 +1,10 @@
 package httpRequest;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /* Esta clase se encarga de realizar efectivamente el pedido de feed al servidor de noticias
  * Leer sobre como hacer una http request en java
@@ -7,15 +12,30 @@ package httpRequest;
  * */
 
 public class httpRequester {
-	
-	public String getFeedRss(String urlFeed){
-		String feedRssXml = null;
-		return feedRssXml;
+	/**
+     * Hace una petición GET al endpoint RSS y devuelve el XML como String.
+     */
+	public String getFeedRss(String urlFeed) throws IOException, InterruptedException {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(urlFeed))
+            .GET()
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
 	}
 
-	public String getFeedReedit(String urlFeed) {
-		String feedReeditJson = null;
-		return feedReeditJson;
-	}
+	/**
+     * Hace una petición GET al endpoint “reedit” y devuelve el JSON como String.
+     */
+	public String getFeedReedit(String urlFeed) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(urlFeed))
+            .GET()
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
 
 }
